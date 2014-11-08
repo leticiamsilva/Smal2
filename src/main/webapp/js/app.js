f87ce83b267c54e76c1f770d443214ad60349fbc
campisano <none@null.na>
2014-11-07 22:06:35 -0200

    angular controller refactoring

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -41,91 +41,76 @@ appModule.config( [
         when("/account/:action*",
         {
             templateUrl: "partials/account.html",
-            controller: "AccountController",
             auth_required: false
         }).
         when("/user/register",
         {
             templateUrl: "partials/register_user.html",
-            controller: "RegisterUserPresenter",
             //auth_required: true //TODO [CMP] re-enable
         }).
         when("/user/list",
         {
             templateUrl: "partials/list_users.html",
-            controller: "ListUsersPresenter",
             //auth_required: true //TODO [CMP] re-enable
         }).
         when("/laboratory/register",
         {
             templateUrl: "partials/register_laboratory.html",
-            controller: "RegisterLaboratoryPresenter",
             //auth_required: true //TODO [CMP] re-enable
         }).
         when("/laboratory/list",
         {
             templateUrl: "partials/list_laboratories.html",
-            controller: "ListLaboratoriesPresenter",
             //auth_required: true //TODO [CMP] re-enable
         }).
         when("/computer/register/:laboratory_name*",
         {
             templateUrl: "partials/register_computer.html",
-            controller: "RegisterComputerPresenter",
             //auth_required: true //TODO [CMP] re-enable
         }).
         when("/computer/list/:laboratory_name*",
         {
             templateUrl: "partials/list_computers.html",
-            controller: "ListComputersPresenter",
             //auth_required: true //TODO [CMP] re-enable
         }).
         when("/trouble/register",
         {
             templateUrl: "partials/register_trouble.html",
-            controller: "RegisterTroublePresenter",
             //auth_required: true //TODO [CMP] re-enable
         }).
         when("/trouble/list",
         {
             templateUrl: "partials/list_troubles.html",
-            controller: "ListTroublesPresenter",
             //auth_required: true //TODO [CMP] re-enable
         }).
         when("/subtrouble/register/:trouble_name*",
         {
             templateUrl: "partials/register_subtrouble.html",
-            controller: "RegisterSubTroublePresenter",
             //auth_required: true //TODO [CMP] re-enable
         }).
         when("/subtrouble/list/:trouble_name*",
         {
             templateUrl: "partials/list_subtroubles.html",
-            controller: "ListSubTroublesPresenter",
             //auth_required: true //TODO [CMP] re-enable
         }).
         when("/ticket/open/:computer_asset_code*",
         {
             templateUrl: "partials/open_ticket.html",
-            controller: "OpenTicketPresenter",
             //auth_required: true //TODO [CMP] re-enable
         }).
         when("/ticket/list",
         {
             templateUrl: "partials/list_tickets.html",
-            controller: "ListTicketsPresenter",
             //auth_required: true //TODO [CMP] re-enable
         }).
         when("/ticket/assign/:protocol*",
         {
             templateUrl: "partials/assign_ticket.html",
-            controller: "AssignTicketPresenter",
             //auth_required: true //TODO [CMP] re-enable
         }).
         when("/ticket/close/:protocol*",
         {
             templateUrl: "partials/close_ticket.html",
-            controller: "CloseTicketPresenter",
             //auth_required: true //TODO [CMP] re-enable
         }).
         otherwise(
