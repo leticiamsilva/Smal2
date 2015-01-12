10b6cda714cbe5e5d69eb2a8bccb570111e65cf3
campisano <none@null.na>
2015-01-12 00:44:54 -0200

    Removed obsolete session_id structure

--- src/main/java/org/smal2/infrastructure/presentation/view/http/util/OperationRequest.java
+++ src/main/java/org/smal2/infrastructure/presentation/view/http/util/OperationRequest.java
@@ -1,16 +1,7 @@
 package org.smal2.infrastructure.presentation.view.http.util;
 
 public class OperationRequest<T> {
-	private String session_id;
 	private T request;
-
-	public String getSession_id() {
-		return session_id;
-	}
-
-	public void setSession_id(String session_id) {
-		this.session_id = session_id;
-	}
 
 	public T getRequest() {
 		return request;
